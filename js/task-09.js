8f3d93702978c007c32ab572af725c681f7971c1
Mylymuk Oksana <oksana.mylymukk@gmail.com>
2022-09-01 01:14:00 +0300

    finished task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const color = document.querySelector("span.color");
+const btn = document.querySelector("button.change-color");
+
+const changeColor = (event) => {
+  color.textContent = getRandomHexColor();
+  body.style.backgroundColor = color.textContent;
+};
+
+btn.addEventListener("click", changeColor);
