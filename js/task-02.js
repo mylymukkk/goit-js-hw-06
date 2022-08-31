40df0340ef1756792d5409bd330e789e5645e802
Mylymuk Oksana <oksana.mylymukk@gmail.com>
2022-08-31 03:45:14 +0300

    finished task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("ul#ingredients");
+
+const fullList = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.classList.add("item");
+  item.textContent = ingredient;
+  return item;
+});
+
+list.append(...fullList);
