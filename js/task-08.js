5105902013e5dcf927c0146a4964e187d1c29c44
Mylymuk Oksana <oksana.mylymukk@gmail.com>
2022-09-01 00:51:17 +0300

    finished task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector("form.login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Please fill in all the fields!");
+    return;
+  }
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+
+  event.currentTarget.reset();
+
+  console.log(formData);
+}
